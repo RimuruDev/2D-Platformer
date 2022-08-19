53b6ea45d71c21f3b7a1fa9e84d6695abe8f67bc
Rimuru <Rimuru.Dev@gmail.com>
2022-08-19 23:19:00 +0300

    Create PlayerInputHandler.cs

---
+++ Simple 2D Platformer/Assets/InternalAssets/Scripts/Core/PlayerInputHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace RimuruDev.Core
+{
+    public sealed class PlayerInputHandler
+    {
+        public void MotionX(Rigidbody2D rigidbody2D, float horizontaiInput, float motionSpeed)
+        {
+            rigidbody2D.velocity = new(horizontaiInput * motionSpeed, rigidbody2D.velocity.y);
+        }
+
+        public void Jump(Rigidbody2D rigidbody2D, float jumpForce)
+        {
+            if (Input.GetButtonDown("Jump"))
+                rigidbody2D.velocity = new(rigidbody2D.velocity.x, jumpForce);
+        }
+
+        public float GetHorizontalInput() => Input.GetAxisRaw("Horizontal");
+
+        public float GetVerticalInput() => Input.GetAxisRaw("Vertical");
+    }
+}
