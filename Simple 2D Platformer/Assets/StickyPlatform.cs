0a580924298ddf2a2765a6e66e234cd1535b1a6d
Rimuru <Rimuru.Dev@gmail.com>
2022-08-20 14:10:40 +0300

    Update StickyPlatform.cs

--- Simple 2D Platformer/Assets/StickyPlatform.cs
+++ Simple 2D Platformer/Assets/StickyPlatform.cs
@@ -1,16 +1,16 @@
 using RimuruDev.Core;
 using UnityEngine;
 
-public class StickyPlatform : MonoBehaviour
+public sealed class StickyPlatform : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.gameObject.CompareTag(Tag.Player)) return;
 
         collision.gameObject.transform.SetParent(transform);
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.gameObject.CompareTag(Tag.Player)) return;
 
